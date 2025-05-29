9d35493b28d264fb0633c713734d4b19c8cb442e
Teteu2024 <matheusmurija863@gmail.com>
2025-05-29 17:31:48 -0300

    continuação da função novos intens criando o objeto de cada iten para ser adicionado ao array de items

--- main.js
+++ main.js
@@ -2,4 +2,13 @@ const items = []
 
 function addItem() {
     const itemName = document.querySelector("#item"). value 
+
+    const item = {
+        name: itemName,
+        checked:false
+    }
+
+    items.push(item)
+
+    console.log(items)
 }
