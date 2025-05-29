0d708a1ec7828eb1e1927c0ea06fc6d5f3928050
Teteu2024 <matheusmurija863@gmail.com>
2025-05-29 19:48:00 -0300

    verificação da input se o checked é verdadeiro ou não

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function showItemsList() {
        sectionList.innerHTML += `     
        <div class="item">
                 <div>
-                    <input type="checkbox" name="list" id="item-${index}">
+                    <input type="checkbox" name="list" id="item-${index}" ${item.checked && "checked"}>
             
                 <div class="custom-checkbox" onclick="checkItem('${item.name}')">
                    <img src="./assets/checked.svg" alt="checked">
